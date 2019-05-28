857fa2141974d4e5b1ab0e66a8779485ef675694
Onyenezido Chukwuemeka Victor <sleonvictor@gmail.com>
2019-05-28 14:31:34 +0100

    Added dev packages for testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { create } from "react-test-renderer";
 import App from './App';
+import { MockedProvider } from 'react-apollo/test-utils';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  create(
+    <MockedProvider mocks={[]}>
+      <App />
+    </MockedProvider>);
 });
